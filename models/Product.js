eb9c62cd7623828accd205d8b55f219072ac482e
Shashi <shashidhar.rao@newstreettech.com>
2024-09-04 08:10:55 +0530

    added stock

--- models/Product.js
+++ models/Product.js
@@ -12,10 +12,10 @@ const productSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
-//   stock: {
-//     type: Number,
-//     required: true,
-//   },
+  stock: {
+    type: Number,
+    required: true,
+  },
   category: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Category',
